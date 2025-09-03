ba07cf64c0352131c307eb616750343979bb1b80
RaulBufit <raulmcdel79@gmail.com>
2025-09-03 16:03:01 +0200

    Hero: nueva imagen de portada y cache bust (?v=2)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,7 +28,7 @@ const Hero: React.FC = () => {
   return (
                 <section 
                     className="relative min-h-screen flex flex-col items-center justify-center text-center px-6 overflow-hidden bg-cover bg-center bg-fixed"
-                    style={{ backgroundImage: "url('/imagenes/foto-portada.png')" }}
+                    style={{ backgroundImage: "url('/imagenes/foto-portada.png?v=2')" }}
                 >
         <div className="absolute inset-0 bg-brand-dark/60"></div>
         <div className="relative z-10">
